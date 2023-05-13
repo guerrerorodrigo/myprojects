64142bbb1c060b48bc0daf89104c44c875da668b
Rodrigo Guerrero <rodrigo.guerrero@warnermedia.com>
2023-05-13 11:29:57 +0200

    Add theme color and font.

--- app/src/main/java/com/rodrigoguerrero/myprojects/ui/theme/Color.kt
+++ app/src/main/java/com/rodrigoguerrero/myprojects/ui/theme/Color.kt
@@ -1,11 +1,61 @@
 package com.rodrigoguerrero.myprojects.ui.theme
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import javax.annotation.concurrent.Immutable
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+private val Purple = Color(0xFF878DFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary
+private val BrightPest = Color(0xFF3FEBF4)
+private val Pest = Color(0xFF34C8DE)
+private val BrightPink = Color(0xFFFF58A6)
+private val Pink = Color(0xFFEC4899)
+private val Yellow = Color(0xFFFBAE16)
+private val Orange = Color(0xFFF9771D)
+private val BrightGreen = Color(0xFF9AF05D)
+private val Grey1 = Color(0xFFE2E8F0)
+private val Grey2 = Color(0xFFF6F5F7)
+private val SoftGrey = Color(0xFFF8FAFC)
+
+// Priority
+private val Violet = Color(0xFFBB6CF9)
+private val Blue = Color(0xFF57A4FF)
+private val Green = Color(0xFF45D2B2)
+
+// Text
+private val Black = Color(0xFF31394F)
+private val White = Color(0xFFFFFFFF)
+
+// State
+private val Info = Color(0xFF64BEF1)
+private val Success = Color(0xFF5FD788)
+private val Warning = Color(0xFFFCDB66)
+private val Error = Color(0xFFF05A5A)
+
+@Immutable
+data class MyProjectsColors(
+    val purple: Color = Purple,
+    val brightPest: Color = BrightPest,
+    val pest: Color = Pest,
+    val brightPink: Color = BrightPink,
+    val pink: Color = Pink,
+    val yellow: Color = Yellow,
+    val orange: Color = Orange,
+    val brightGreen: Color = BrightGreen,
+    val grey1: Color = Grey1,
+    val grey2: Color = Grey2,
+    val softGrey: Color = SoftGrey,
+    val violet: Color = Violet,
+    val blue: Color = Blue,
+    val green: Color = Green,
+    val black: Color = Black,
+    val white: Color = White,
+    val info: Color = Info,
+    val success: Color = Success,
+    val warning: Color = Warning,
+    val error: Color = Error
+)
+
+val LocalColors = staticCompositionLocalOf { MyProjectsColors() }
--- app/src/main/java/com/rodrigoguerrero/myprojects/ui/theme/Type.kt
+++ app/src/main/java/com/rodrigoguerrero/myprojects/ui/theme/Type.kt
@@ -1,34 +1,183 @@
 package com.rodrigoguerrero.myprojects.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rodrigoguerrero.myprojects.R
+import javax.annotation.concurrent.Immutable
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+private val Inter = FontFamily(
+    Font(resId = R.font.font_inter, style = FontStyle.Normal)
+)
+
+@Immutable
+data class MyProjectsTypography(
+    val heading1: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 26.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight.Bold,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+    val heading2: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val heading3: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val heading4: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val heading5: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 18.sp,
+        lineHeight = 28.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val heading6: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 15.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val heading7: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val heading8: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val heading9: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val heading10: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val titleButton: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val body1: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 13.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val body2: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 13.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val body3: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val body4: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    val body5: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    val body6: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val body7: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val body8: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val body9: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val body10: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 12.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val caption1: TextStyle = TextStyle(
+        fontFamily = Inter,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontWeight = FontWeight.Bold
+    ),
+    val caption2: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val caption3: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 8.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    val caption4: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 8.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val caption5: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 7.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    val caption6: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontSize = 7.sp,
+        lineHeight = 12.sp,
+        fontWeight = FontWeight.Normal
+    ),
 )
+
+val LocalTypography = staticCompositionLocalOf { MyProjectsTypography() }
