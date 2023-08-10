724f548785821c66182f8f07f6819ac4b884c6b4
Rodrigo Guerrero <rodrigo.guerrero@warnermedia.com>
2023-08-10 10:37:50 +0200

    Fix

--- build-logic/convention/src/main/kotlin/com/rodrigoguerrero/convention/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/com/rodrigoguerrero/convention/AppConfig.kt
@@ -4,11 +4,11 @@ import org.gradle.api.JavaVersion
 
 object AppConfig {
     const val applicationId = "com.rodrigoguerrero.myprojects"
-    const val compileSdk = 33
+    const val compileSdk = 34
     val javaVersion = JavaVersion.VERSION_17
     const val minSdk = 23
     const val namespace = "com.rodrigoguerrero.myprojects"
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val version = "1.0.0"
     const val versionCode = 1
 }
